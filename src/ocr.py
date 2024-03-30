83d0acd11664221abdbfefbb411df66d07dafda2
Martino <domiziano.martino@gmail.com>
2024-03-31 00:18:43 +0100

    fix: open image

--- src/ocr.py
+++ src/ocr.py
@@ -7,14 +7,17 @@ from image_processor import ImagePreprocessor
 
 class OCRProcessor:
     def find_yellow_highlights_and_extract_text(self, image):
-        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        open_cv_image = np.array(image)
+        open_cv_image = open_cv_image[:, :, ::-1].copy()
+
+        hsv = cv2.cvtColor(open_cv_image, cv2.COLOR_BGR2HSV)
         lower_yellow = np.array([22, 93, 100])
         upper_yellow = np.array([45, 255, 255])
         mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
 
         dilated_mask = ImagePreprocessor.dilate_mask(mask)
 
-        masked_image = cv2.bitwise_and(image, image, mask=dilated_mask)
+        masked_image = cv2.bitwise_and(open_cv_image, open_cv_image, mask=dilated_mask)
         preprocessed_image = ImagePreprocessor.preprocess_image_for_ocr(masked_image)
 
         custom_config = r"--oem 3 --psm 6 -l ita"
