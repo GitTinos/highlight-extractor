3b1be7a157635ed47bf736b7d1b0dd814f998fe6
mdomiziano <mdomiziano@it.iliad.como>
2024-04-03 21:52:35 +0200

    feat: add progress bar

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import argparse
 
 from pdf_manager import PDFManager
 from ocr import OCRProcessor
-from utils import clean_text
+from utils import clean_text, start_progress_bar
 
 
 def main():
@@ -12,31 +12,31 @@ def main():
     parser.add_argument("pdf_path", help="The path to the PDF file to analyze.")
     args = parser.parse_args()
 
-    # Ottiene il numero totale di pagine usando il PDFManager
     total_pages = PDFManager.get_total_pages(args.pdf_path)
-    print("total pages", total_pages)
-
     ocr_processor = OCRProcessor()
     extractions = []
 
-    page_block_size = 50  # Dimensione del blocco di pagine
+    progress_bar = start_progress_bar(total_pages)
+
+    page_block_size = 50
     start_page = 1
 
     while start_page <= total_pages:
         end_page = min(start_page + page_block_size - 1, total_pages)
-        print("l'ultima pagina del blocco", end_page)
         images = PDFManager.convert_pdf_to_images(
             args.pdf_path, start_page=start_page, end_page=end_page
         )
 
         for page_num, image in enumerate(images, start=start_page):
-            print(f"Elaborazione pagina {page_num}")
             text = ocr_processor.find_yellow_highlights_and_extract_text(image)
             cleaned_text = clean_text(text)
             if cleaned_text:
                 extractions.append(f"Page {page_num}: {cleaned_text}")
+            progress_bar.update(1)
 
-        start_page += page_block_size  # Prepara il blocco successivo
+        start_page += page_block_size
+
+    progress_bar.close()
 
     with open("text_extractions.txt", "w") as file:
         for extraction in extractions:
